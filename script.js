fede1f3ee550df18fc34daf38ba2ff92d3435858
glezzz <alexglezk@gmail.com>
2020-09-15 10:15:14 +0200

    search function works

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-document.getElementById("searchBtn").addEventListener("click", ()=> {
+document.getElementById("search-bar").addEventListener("click", () => {
 
     let input = document.getElementById("search").value;
-    run.addEventListener("click", function (){
-    fetch("https://pokeapi.co/api/v2/pokemon/1" + input)
+
+    fetch("https://pokeapi.co/api/v2/pokemon/" + input)
         .then(response => response.json())
-        .then(data => console.log(data));
-        document.getElementById("id").innerHTML = `${data.id}`;
-        document.getElementById("image").setAttribute("src", `${data.sprites.front_default}`)
-        document.getElementById("pokemonName").innerHTML = `${data.name}`;
+        .then(data => {
+            console.log(data);
+            document.getElementById("id").innerHTML = `${data.id}`;
+            document.getElementById("image").setAttribute("src", `${data.sprites.front_default}`);
+            document.getElementById("pokemonName").innerHTML = `${data.name}`;
+        })
 });
 
