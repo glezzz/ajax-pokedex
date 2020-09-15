94045e9fdc57ad8e4030f1eba0d5a399f21659f1
glezzz <alexglezk@gmail.com>
2020-09-15 09:45:44 +0200

    almost have name id

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
+document.getElementById("searchBtn").addEventListener("click", ()=> {
+
+    let input = document.getElementById("search").value;
+    run.addEventListener("click", function (){
     fetch("https://pokeapi.co/api/v2/pokemon/1" + input)
         .then(response => response.json())
         .then(data => console.log(data));
+        document.getElementById("id").innerHTML = `${data.id}`;
+        document.getElementById("image").setAttribute("src", `${data.sprites.front_default}`)
+        document.getElementById("pokemonName").innerHTML = `${data.name}`;
+});
+
